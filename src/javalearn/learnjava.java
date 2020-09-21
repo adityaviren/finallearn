684f4aea594c3cb357e09388b209a80a6eb8d266
adityaviren <adityaviren@gmail.com>
2020-09-21 19:12:01 +0530

    First commit

---
+++ src/javalearn/learnjava.java
@@ -0,0 +1,21 @@
+package javalearn;
+
+import java.util.Scanner;
+
+public class learnjava {
+
+	public static void main(String[] args) {
+		learnjava lj=new learnjava();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the 1st number");
+		int num1=sc.nextInt();
+		System.out.println("Enter the 2nd number");
+		int num2=sc.nextInt();
+		lj.add2numbers(num1,num2);
+	}
+	private void add2numbers(int a,int b) {
+		long sum=a+b;
+		System.out.println("Addition of 2 numbers is "+sum);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module javalearn {
+}
