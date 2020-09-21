e5f6651055a56f5f10f86dcd7745adc0f2502fd4
adityaviren <51331064+adityaviren@users.noreply.github.com>
2020-09-21 19:16:34 +0530

    Update learnjava.java

--- src/javalearn/learnjava.java
+++ src/javalearn/learnjava.java
@@ -17,5 +17,9 @@ public class learnjava {
 		long sum=a+b;
 		System.out.println("Addition of 2 numbers is "+sum);
 	}
+	private void sub2numbers(int a, int b) {
+		lond sub=a-b;
+		System.out.println("Subtraction of 2 numbers is "+sub);
+	}
 
 }
